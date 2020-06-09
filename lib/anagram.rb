e326a32ffdc3a567844ca067f6c06e56669ee4fe
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-09 01:57:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,60 @@
-# Your code goes here!
-require 'pry'
 
-class Anagram
-    attr_accessor :word
 
-    def initialize(word)
-        @word = word 
-    end
-    def match(array)
-        array.select {|array1| array1.split("").sort == @word.split("").sort}
-        # binding.pry 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Your code goes here!
+# require 'pry'
+
+# class Anagram
+#     attr_accessor :word
+
+#     def initialize(word)
+#         @word = word 
+#     end
+#     def match(array)
+#         array.select {|array1| array1.split("").sort == @word.split("").sort}
+#         # binding.pry 
         
-    end
-end
+#     end
+# end
